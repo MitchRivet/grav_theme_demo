0309dadc47aba7c2c476ffbe5a40cb82ceb01e32
mitch <student@epicodus.com>
2015-11-13 16:07:10 -0800

    initial commit

---
+++ cache/compiled/files/f9becdecd15363182bd64acc219f62fa.yaml.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/Guest/Desktop/grav_custom_theme/user/themes/antimatter/languages.yaml',
+    'modified' => 1446207158,
+    'data' => [
+        'en' => [
+            'TRANSLATION_TEST' => 'Antimatter!'
+        ]
+    ]
+];
